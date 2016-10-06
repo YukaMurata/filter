7413c4424846ea17de985055696b5ed0c0943e1c
murata-yuka <murata-yuka@9105-109520.local>
2016-10-06 18:06:17 +0900

    resetボタン実装

--- js/practice.js
+++ js/practice.js
@@ -1,32 +1,75 @@
 $(function(){
 
-$('input[name=num]').on('click',function() {
+	var $contentListItem = $("#content_list li");
+
+
+	var showingClass = []; 
+
+	$('input[name=num]').on('change',function() {
+		var val = $(this).val();
+		updateShowingClass(val);
+		showList();
+
 
 	//$(".content_list_item").velocity("fadeOut",{duration:150});
-$(this).each(function(){
+		// $(this).each(function(){
+			
 
-	if($(this).prop("checked")){
-	//	var n = $("#check_btn :checkbox:checked").length;
-		$("#content_list li").velocity("fadeOut",
-	 {duration:350
-	    });
-    var val = $(this).val();
-    
-    console.log(val);
+		// 	if($(this).prop("checked")){
+		// //	var n = $("#check_btn :checkbox:checked").length;
+		// 		$("#content_list li").velocity("fadeOut",{
+		// 			duration:350,
+		// 			complete:function(){
+		// 				$("#content_list .content_list_item." + val ).velocity("fadeIn",{
+		// 					duration:350
+		// 				});
+		// 			}
+		// 		});
+		// 		console.log(val);
 
-	 $("#content_list .content_list_item." + val ).velocity("fadeIn",
-	 {duration:350
-	    });
-	} else{
-		$("#content_list li").velocity("fadeIn",
-	 {duration:350
-	    });
+				
+		// 	} else{
+		// 		$contentListItem.velocity("fadeIn",
+		// 			{duration:350
+		// 		});
+		// 	}
+
+		// });
+
+	 });
+
+	function updateShowingClass (val){
+		showingClass.push(val);
+		console.log(showingClass);
 	}
 
- });
+	function showList (){
+		var $list = $("." + showingClass.join("."));
+
+		$("#content_list li").velocity("fadeOut",{
+		 			duration:350,
+		 			complete:function(){
+		 				$list.velocity("fadeIn",{
+		 					duration:350
+		 				});
+		 			}
+		 		});
+	}
+
+	$('input[name=reset]').on('click',function() {
+		resetShowingClass();
+		$('input[name=num]').prop('checked',false);
+	});
+
+	function resetShowingClass(){
+		showingClass = [];
+		$("#content_list li").velocity("fadeIn",{
+		 					duration:350
+		 				});
+		 			}
+
 
 });
-
 /*$('input[name="age"]:checked').each(function(){
 	var check_val= $(this).val;
   console.log(check_val);
@@ -44,4 +87,4 @@ $(this).each(function(){
     });*/
 
 
-});
+
